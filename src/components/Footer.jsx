ec8e1bed9f42fde253ee1705e78fb25de2648d6d
Subhradip Mishra <mishrasubhradip2005@gmail.com>
2025-05-16 17:14:25 +0530

    add name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-black text-yellow-400 py-6 mt-auto">
       <div className="container mx-auto flex flex-col sm:flex-row justify-center sm:justify-between items-center space-y-4 sm:space-y-0 px-4">
-        <p>&copy; {new Date().getFullYear()} ShopSphere. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} SubhradipMishra. All rights reserved.</p>
         <nav className="flex space-x-6 text-sm">
           <Link to="/privacy-policy" className="hover:underline">
             Privacy Policy
